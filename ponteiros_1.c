e7c3695f7d6c484ce034c693c928feae01303615
Vini <contavin@live.com>
2024-05-16 08:59:34 -0300

    matrizes

--- ponteiros_1.c
+++ ponteiros_1.c
@@ -150,6 +150,128 @@ void str_cat_test()
 
 
 }
+// Parte 3. Matrizes
+//-func-prototypes---------------------------------------------
+void print_matrix ()
+{
+
+    
+
+
+
+}
+void matriz_identidade(int n)
+{
+    int mat_id[n][n];
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+            if (i == j)
+                mat_id[i][j] = 1;
+            else
+                mat_id[i][j] = 0;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+            printf ("[%d]", mat_id[i][j]);
+
+        putchar ('\n');
+    }
+
+
+    //printa operando ponteiro, mas printa tudo em uma linha só
+    //por que razão preciso subtrair o n pra funcionar?
+    int *p;
+
+    p = &mat_id[0][0];
+
+    while (p < &mat_id[n][n]-n)
+    {  
+        printf ("{%d}%p\n", *p, p);
+        ++p;
+    }
+
+    putchar ('\n');
+
+    //tentando passar o início de cada linha
+
+    int (*pp)[n], *q;
+    pp = mat_id;
+
+
+    q = *pp;
+
+    //printa operando ponteiro com linhas e colunas, mas com laço for
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+            printf ("|%d|", *(q+j));
+
+        putchar ('\n');
+        
+        ++pp;
+        q = *pp;
+
+    }
+
+}
+void matriz_densidade(int n)
+{
+    int mat_dens_2d[n][n];
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            mat_dens_2d[i][j] = 0;
+
+    for (int x = 0; x < n-1; ++x)
+        for (int i = x; i < n-x; ++i)
+            for (int j = x; j < n-x; ++j)
+                ++mat_dens_2d[i][j];
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+            printf ("[%d]", mat_dens_2d[i][j]);
+        
+        putchar ('\n');
+    }
+
+}
+void matriz_piramide(int n)
+{
+
+
+
+
+}
+void matriz_piramide_horizontal(int n)
+{
+    int mat_dens_2d[n][n];
+
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            mat_dens_2d[i][j] = 0;
+    
+    //se tirar o laço do h, ele vai só representar onde a pirâmide existe (1). Se trocar o I pelo J, pirâmide "gira" 90 graus
+   for (int h = 0; h < n; ++h)
+        for (int j = h; j < n-h; ++j)
+            for (int i = j; i < n-j; ++i)
+                ++mat_dens_2d[i][j];
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+            printf ("[%d]", mat_dens_2d[i][j]);
+        
+        putchar ('\n');
+    }
+
+}
+//-func-tests--------------------------------------------------
+
 //-main-exec------------------------------------------------------
 int main()
 {
@@ -185,7 +307,39 @@ int main()
     //sentence[x+i+1] = '\0';
     //printf ("%s\n", sentence);
 
+    /*for (int j = 0; j < 10; ++j)
+    {
+        for (int i = 0; i < 10; ++i)
+            if (i == j)
+                mat_id[i][j] = 1;
+            else
+                mat_id[i][j] = 0;
+    }
 
-    str_cat_test();
+    for (int j = 0; j < 10; ++j)
+    {
+        for (int i = 0; i < 10; ++i)
+            printf ("[%d]", mat_id[i][j]);
+        
+        putchar ('\n');
+    } */
+
+    //int tam_matriz = 0;
+//
+    //scanf ("%d", &tam_matriz);
+//
+    //matriz_identidade(tam_matriz);
+
+    int n = 0;
+
+    scanf ("%d", &n);
+
+    matriz_identidade(n);
+    putchar ('\n');
+    matriz_piramide_horizontal(n);
+    putchar ('\n');
+    matriz_densidade(n);
+    putchar ('\n');
+        
 
 }
